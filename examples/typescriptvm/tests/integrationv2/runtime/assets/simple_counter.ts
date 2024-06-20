092a9ca71947e89ca33833438f77af64b57d7951
containerman17 <whatever@whatever.com>
2024-06-20 02:06:36 +0000

    runtime tests passed

---
+++ examples/typescriptvm/tests/integrationv2/runtime/assets/simple_counter.ts
@@ -0,0 +1,74 @@
+import { registerFunc, encoders, GetBytesFunc, SetBytesFunc, execute } from "../../../../runtime/js_sdk";
+import { BigintToUint8Array, Uint8ArrayToBigint } from "../../../../runtime/js_sdk/src/encoders";
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+registerFunc("read", (_payload: Uint8Array, actor: Uint8Array, getBytes: GetBytesFunc, _setBytes: SetBytesFunc) => {
+    const balanceBytes = getBytes(new Uint8Array(actor), 10)
+    const balance = Uint8ArrayToBigint(balanceBytes)
+
+    return encoder.encode(balance.toString());
+})
+
+registerFunc("increment", (payload: Uint8Array, actor: Uint8Array, getBytes: GetBytesFunc, setBytes: SetBytesFunc) => {
+    const value = payload[0];
+
+    const balanceBytes = getBytes(new Uint8Array(actor), 10)
+    const balance = Uint8ArrayToBigint(balanceBytes)
+    const newBalance = balance + BigInt(value)
+    setBytes(new Uint8Array(actor), 10, BigintToUint8Array(newBalance))
+
+    return new Uint8Array();
+})
+
+registerFunc("echo", (payload: Uint8Array, _actor: Uint8Array, _getBytes: GetBytesFunc, _setBytes: SetBytesFunc) => {
+    const value = payload[0];
+
+    return encoder.encode(JSON.stringify(value));
+})
+
+
+registerFunc("writeManySlots", (payload: Uint8Array, _actor: Uint8Array, _getBytes: GetBytesFunc, setBytes: SetBytesFunc) => {
+    const value = payload[0];
+
+    for (let i = 1; i <= value; i++) {
+        setBytes(new Uint8Array([i]), 6, Uint8Array.from([i, i, i]));
+    }
+
+    return new Uint8Array();
+})
+
+
+registerFunc("readManySlots", (payload: Uint8Array, _actor: Uint8Array, getBytes: GetBytesFunc, _setBytes: SetBytesFunc) => {
+    const value = payload[0];
+
+    let result = new Uint8Array();
+    for (let i = 0; i < value; i++) {
+        const fromSlot = getBytes(new Uint8Array([i + 1]), 6);
+        let tempResult = new Uint8Array(result.length + fromSlot.length);
+        tempResult.set(result);
+        tempResult.set(fromSlot, result.length);
+        result = tempResult;
+    }
+    return result;
+})
+
+registerFunc("loadCPU", (payload: Uint8Array, _actor: Uint8Array, _getBytes: GetBytesFunc, _setBytes: SetBytesFunc) => {
+    const value = payload[0];
+
+    const memoryBalloon = new Array(value * value * 1024);
+    for (let i = 0; i < value; i++) {
+        for (let j = 0; j < value; j++) {
+            for (let k = 0; k < value; k++) {
+                memoryBalloon[i * j * k] = i * j * k;
+            }
+        }
+    }
+    const summ = memoryBalloon.reduce((acc, curr) => acc + curr, 0);
+    return encoder.encode(JSON.stringify(summ));
+})
+
+
+
+execute();
---
+++ examples/typescriptvm/tests/integrationv2/runtime/return_test.go
@@ -0,0 +1,128 @@
+package runtime_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ava-labs/hypersdk/examples/typescriptvm/runtime"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReturn(t *testing.T) {
+	exec := runtime.NewJavyExec()
+	stateProvider := runtime.NewDummyStateProvider()
+
+	actor1Bytes := createActorAddress(1)
+	actor2Bytes := createActorAddress(2)
+	actor3Bytes := createActorAddress(3)
+
+	params := runtime.JavyExecParams{
+		MaxFuel:       10 * 1000 * 1000,
+		MaxTime:       time.Millisecond * 100,
+		MaxMemory:     1024 * 1024 * 100,
+		Bytecode:      &testWasmBytes,
+		StateProvider: stateProvider.StateProvider,
+		Payload:       []byte{7},
+		FunctionName:  "increment",
+	}
+
+	//execute 2 times for actor 1
+	params.Actor = actor1Bytes
+	for i := 0; i < 2; i++ {
+		res, err := exec.Execute(params)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		require.Equal(t, true, res.Result.Success)
+
+		fmt.Printf("UpdatedKeys: %v\n", res.Result.UpdatedKeys)
+		stateProvider.Update(res.Result.UpdatedKeys)
+	}
+
+	//execute 1 time for actor 2
+	params.Actor = actor2Bytes
+	res, err := exec.Execute(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stateProvider.Update(res.Result.UpdatedKeys)
+
+	//get result for actor 1
+	params.Payload = []byte{}
+	params.FunctionName = "read"
+	params.Actor = actor1Bytes
+
+	res, err = exec.Execute(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stateProvider.Update(res.Result.UpdatedKeys)
+
+	//check result for actor 1
+	var actor1FinalBalance uint32
+	if err := json.Unmarshal(res.Result.Result, &actor1FinalBalance); err != nil {
+		t.Fatal(err)
+	}
+	if actor1FinalBalance != 7*2 { //2 increments, 7 each
+		t.Fatalf("Expected balance %d, got %d", 7*2, actor1FinalBalance)
+	}
+
+	//get result for actor 2
+	params.Payload = []byte{}
+	params.FunctionName = "read"
+	params.Actor = actor2Bytes
+	res, err = exec.Execute(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//check result for actor 2
+	var actor2FinalBalance uint32
+	if err := json.Unmarshal(res.Result.Result, &actor2FinalBalance); err != nil {
+		t.Fatal(err)
+	}
+
+	if actor2FinalBalance != 7 {
+		t.Fatalf("Expected balance %d, got %d", 7, actor2FinalBalance)
+	}
+
+	//check zero balance for actor 3
+	params.Payload = []byte{}
+	params.FunctionName = "read"
+	params.Actor = actor3Bytes
+	res, err = exec.Execute(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var actor3FinalBalance uint32
+	if err := json.Unmarshal(res.Result.Result, &actor3FinalBalance); err != nil {
+		t.Fatal(err)
+	}
+
+	if actor3FinalBalance != 0 {
+		t.Fatalf("Expected balance %d, got %d", 0, actor3FinalBalance)
+	}
+}
+
+func TestEcho(t *testing.T) {
+	exec := runtime.NewJavyExec()
+	params := DEFAULT_PARAMS_LIMITS
+	params.Payload = []byte{124}
+	params.FunctionName = "echo"
+	res, err := exec.Execute(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var result uint32
+	if err := json.Unmarshal(res.Result.Result, &result); err != nil {
+		t.Fatal(err)
+	}
+	if result != 124 {
+		t.Fatalf("Expected result %d, got %d", 124, result)
+	}
+}
