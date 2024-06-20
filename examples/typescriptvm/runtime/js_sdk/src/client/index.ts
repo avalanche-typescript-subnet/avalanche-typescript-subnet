b8f3c0462a287e38922bd700948eb7d2277fb892
containerman17 <solohin.i@gmail.com>
2024-06-20 13:03:30 +0000

    move js sdk into runtime subfolder

---
+++ examples/typescriptvm/runtime/js_sdk/src/client/index.ts
@@ -0,0 +1,25 @@
+
+
+export class TsChainClient {
+    constructor(private readonly url: string) {
+    }
+
+    public async balance(address: string): Promise<bigint> {
+        const response = await fetch(this.url + "/morpheusapi", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                jsonrpc: "2.0",
+                method: "morpheusvm.balance",
+                params: { address },
+                id: parseInt(String(Math.random()).slice(2))
+            })
+        });
+
+        const json = await response.json();
+        console.log('json', json)
+        return BigInt(json.result.amount);
+    }
+}
